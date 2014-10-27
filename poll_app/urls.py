24ee7ed12aa46fe8376ac165ca020a4e6da4f6b4
exploreshaifali <agrawalshaifali09@gmail.com>
2014-10-27 23:36:20 +0530

    pointing root urlconf to the poll.urls.py

--- poll_app/urls.py
+++ poll_app/urls.py
@@ -8,5 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'poll_app.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^polls/', include('poll.urls')),
+
     url(r'^admin/', include(admin.site.urls)),
 )
