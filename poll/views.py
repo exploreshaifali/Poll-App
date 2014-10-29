c74f75226de504ac0b063e0e7e1967145cb3f0eb
exploreshaifali <agrawalshaifali09@gmail.com>
2014-10-29 14:10:51 +0530

    changed code to make compatable as per current specs.

--- poll/views.py
+++ poll/views.py
@@ -6,11 +6,11 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the poll index.")
 
-def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+def detail(request, question_id):
+    return HttpResponse("You're looking at poll %s." % question_id)
 
-def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+def results(request, question_id):
+    return HttpResponse("You're looking at the results of poll %s." % question_id)
 
-def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)    
+def vote(request, question_id):
+    return HttpResponse("You're voting on poll %s." % question_id)    
