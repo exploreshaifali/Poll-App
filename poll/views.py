2ad5b576961f4a7ab1146c18446b8ba04e33a9a8
exploreshaifali <agrawalshaifali09@gmail.com>
2014-11-02 15:05:39 +0530

    index view of poll is not compatible with its template index

--- poll/views.py
+++ poll/views.py
@@ -7,8 +7,9 @@ from poll.models import Question
 def index(request):
     #return HttpResponse("Hello, world. You're at the poll index.")
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([p.question_text for p in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'poll/index.html', context)
+
 
 def detail(request, question_id):
     return HttpResponse("You're looking at poll %s." % question_id)
