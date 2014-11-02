85c09f7f59a7796f64d012edca5e2b2f46513289
exploreshaifali <agrawalshaifali09@gmail.com>
2014-11-02 14:07:32 +0530

    modified index method of views.py so that it can show details of recent 5 polls.

--- poll/views.py
+++ poll/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from poll.models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    #return HttpResponse("Hello, world. You're at the poll index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([p.question_text for p in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at poll %s." % question_id)
