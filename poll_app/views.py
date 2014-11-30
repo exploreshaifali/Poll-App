01b36a1962d6cf7ba950f6f739ca3ad2d3398c70
exploreshaifali <agrawalshaifali09@gmail.com>
2014-11-30 15:28:03 +0530

    Added proper index page of whole application.

---
+++ poll_app/views.py
@@ -0,0 +1,17 @@
+#For user authentication..
+from django.contrib.auth import logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response
+
+def main_page(request):
+    return render_to_response('index.html')
+
+def home(request):
+	return HttpResponse("Hello Welcome to poll's app.")
+
+def logout_page(request):
+    """
+    Log users out and re-direct them to the main page.
+    """
+    logout(request)
+    return HttpResponseRedirect('/')
