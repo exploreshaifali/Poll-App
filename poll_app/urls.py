f1b5734654786a66c445e4d0905366b60b019f86
exploreshaifali <agrawalshaifali09@gmail.com>
2014-11-30 15:27:29 +0530

    modified application index url

--- poll_app/urls.py
+++ poll_app/urls.py
@@ -5,10 +5,14 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
+    # to make application index as the home page defined in views.
     # url(r'^$', 'poll_app.views.home', name='home'),
+    # to make appliction index as main_page defined in views
+    url(r'^$', 'poll_app.views.main_page', name='main_page'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^polls/', include('poll.urls', namespace='poll')),
-    url(r'^$', include('poll.urls')),
+    # To make poll index page to ne index page of whole application
+    # url(r'^$', include('poll.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
