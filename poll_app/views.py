28811b4634da0539b0be23cf326dea6ef9723e25
exploreshaifali <agrawalshaifali09@gmail.com>
2014-11-30 16:52:07 +0530

    for making index.html as index of whole app

--- poll_app/views.py
+++ poll_app/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import logout
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 
+
 def main_page(request):
     return render_to_response('index.html')
 
