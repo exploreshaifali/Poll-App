b7d131c3a39598f5abe0419f5ab52e4b6c6eff28
exploreshaifali <agrawalshaifali09@gmail.com>
2014-12-01 12:31:11 +0530

    Modified Admin for articles app.
    Added list_display, list_filers and search_fields.

--- articles/admin.py
+++ articles/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from articles.models import Article
 # Register your models here.
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'pub_date', 'likes')
+    list_filter = ['pub_date', 'likes']
+    search_fields = ['title']
+
+admin.site.register(Article, ArticleAdmin)
