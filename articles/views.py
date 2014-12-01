2a58c43b6ecd1ba847a4d8c9d01e4f22262d0865
exploreshaifali <agrawalshaifali09@gmail.com>
2014-12-01 12:44:33 +0530

    Spelling misstake corrected in views.py of articles.

--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from articles.models import Article
 # Create your views here.
 def article_index(request):
-	artiles_list = Article.objects.order_by('pub_date')
-	context = {'artiles_list': artiles_list}
+	articles_list = Article.objects.order_by('pub_date')
+	context = {'articles_list': articles_list}
 	return render(request, 'articles/index.html', context)
