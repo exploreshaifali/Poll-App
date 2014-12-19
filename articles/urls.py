8b3bfb2728cf68d6c0b337063878db000aa46749
exploreshaifali <agrawalshaifali09@gmail.com>
2014-12-19 23:00:31 +0530

    modified articles views to be generic class view

--- articles/urls.py
+++ articles/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from articles import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.article_index, name='index'),
+    # url(r'^$', views.article_index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 )
--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from articles.models import Article
+from django.views import generic
 # Create your views here.
-def article_index(request):
-	'''return the list of articles present in databse'''
-	articles_list = Article.objects.order_by('pub_date')
-	context = {'articles_list': articles_list}
-	return render(request, 'articles/index.html', context)
+
+class IndexView(generic.ListView):
+	template_name = 'articles/index.html'
+	context_object_name = 'articles_list'
+
+	def get_queryset(self):
+		"""Retutn last 9 published articles"""
+		return Article.objects.order_by('pub_date')[:9]
+
+
+# def article_index(request):
+# 	'''return the list of articles present in databse'''
+# 	articles_list = Article.objects.order_by('pub_date')
+# 	context = {'articles_list': articles_list}
+# 	return render(request, 'articles/index.html', context)
+
