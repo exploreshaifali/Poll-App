2d79c4dade610f599f8a92b353a865c78e011208
exploreshaifali <agrawalshaifali09@gmail.com>
2014-12-19 22:44:03 +0530

    added comments for articles_index

--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from articles.models import Article
 # Create your views here.
 def article_index(request):
+	'''return the list of articles present in databse'''
 	articles_list = Article.objects.order_by('pub_date')
 	context = {'articles_list': articles_list}
 	return render(request, 'articles/index.html', context)
