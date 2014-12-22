e0fd97d745dc934dd0f4c7e60bcf0f93f86029fa
exploreshaifali <agrawalshaifali09@gmail.com>
2014-12-23 00:49:33 +0530

    Added login view defination.

--- poll_app/views.py
+++ poll_app/views.py
@@ -2,6 +2,8 @@
 from django.contrib.auth import logout
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
+from django.contrib import auth 
+from django.core.context_processors import csrf
 
 
 def main_page(request):
@@ -10,9 +12,11 @@ def main_page(request):
 def home(request):
 	return HttpResponse("Hello Welcome to poll's app.")
 
-def logout_page(request):
-    """
-    Log users out and re-direct them to the main page.
-    """
-    logout(request)
-    return HttpResponseRedirect('/')
+def login(request):
+	'''if request is vallid not fake move to the login template
+	wich will redirect to auth_view()
+	'''
+	c = {}
+	c.update(csrf(request))
+	return render_to_response('login.html', c)
+
