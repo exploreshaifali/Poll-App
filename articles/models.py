af060a63e362bfb79e2d3a987a33a667d7cde646
exploreshaifali <agrawalshaifali09@gmail.com>
2014-12-27 01:59:23 +0530

    Changed article model to have thumbnail too for each article.

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from time import time
 
-# Create your models here.
+def get_upload_file_name(instance, filename):
+	return "uploaded_files/%s_%s/" % (str(time()).replace('.','_'), filename)
 
 class Article(models.Model):
 	"""docstring for Article"""
@@ -8,6 +10,7 @@ class Article(models.Model):
 	body = models.TextField()
 	pub_date = models.DateTimeField()
 	likes = models.IntegerField(default=0)
+	thumbnail = models.FileField(upload_to=get_upload_file_name)
 
 	def __unicode__(self):
 		return self.title
