c4b9a2248df0e45c8e48bc3e1af53b13ad566381
exploreshaifali <agrawalshaifali09@gmail.com>
2014-12-27 01:53:29 +0530

    Configured settings for MEDIA root and url for user uploads.

--- poll_app/settings.py
+++ poll_app/settings.py
@@ -87,6 +87,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+#Absolute file system part to the directory that will hold user uploaded files.
+MEDIA_ROOT = '/home/shaifali/poll_app/static'
+
+# Url that handle the media served from MEDIA_ROOT. Make sure to use a 
+# trailing slash.
+MEDIA_URL = ''
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
