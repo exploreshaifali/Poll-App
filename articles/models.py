499904db0dd4995841611c6f4155506d4d640e41
exploreshaifali <agrawalshaifali09@gmail.com>
2014-12-26 17:40:35 +0530

    Article model is modified with default likes to zero

--- articles/models.py
+++ articles/models.py
@@ -7,7 +7,7 @@ class Article(models.Model):
 	title = models.CharField(max_length=200)
 	body = models.TextField()
 	pub_date = models.DateTimeField()
-	likes = models.IntegerField()
+	likes = models.IntegerField(default=0)
 
 	def __unicode__(self):
 		return self.title
