eab847a4b10280c9a12c64c6a4334301e1536077
exploreshaifali <agrawalshaifali09@gmail.com>
2014-12-26 17:55:18 +0530

    Added form for Article Model.

---
+++ articles/forms.py
@@ -0,0 +1,11 @@
+# forms file specific to articles app
+
+from django import forms
+from articles.models import Article
+
+class ArticleForm(forms.ModelForm):
+
+	class Meta:
+		"""docstring for Meta"""
+		model = Article
+		fields = ('title', 'body', 'pub_date')
