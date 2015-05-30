bcb4855e22a9f331a8270d832f14169ab5a7160b
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-05-31 02:46:38 +0530

    models added in masterIt to play with cities_light and address.

--- masterIt/models.py
+++ masterIt/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cities_light.models import Country, Region, City
 
 # Create your models here.
 class TryIterables(models.Model):
@@ -9,3 +10,13 @@ class TryIterables(models.Model):
         )
 	name = models.CharField(max_length=60)
 	shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+
+from address.models import AddressField
+class TestAddress(models.Model):
+    address = AddressField()
+
+class CountryStateCity(models.Model):
+    """Practice to validate country, state, city data"""
+    country = models.ForeignKey(Country)
+    region = models.ForeignKey(Region)
+    city = models.ForeignKey(City)
